cd62f3293cdddee2ba47ae6a6a038ce519f53768
rajat-wyrm <rajatkumar7861813@gmail.com>
2026-05-27 19:01:06 +0530

    feat(db): add prisma client singleton

---
+++ src/db/prisma.ts
@@ -0,0 +1,17 @@
+﻿import { PrismaClient } from '@prisma/client';
+import config from '../config/index.js';
+
+const prisma = new PrismaClient({
+  log: config.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+});
+
+export async function connectDB() {
+  await prisma.$connect();
+  console.log('📦 PostgreSQL connected');
+}
+
+export async function disconnectDB() {
+  await prisma.$disconnect();
+}
+
+export default prisma;
